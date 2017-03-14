8a197de712d82bb6aa4b80bbeeeb1ede421967ae
Thogul <thogul@outlook.com>
2017-03-14 15:26:42 +0100

    Added project files
    LET'S GO

---
+++ Sammarbeidsoppgave/Brikker.cs
@@ -0,0 +1,32 @@
+using system;
+
+namespace Sammarbeidsoppgave
+{
+    class Brikker
+    {
+      //Attributter
+      list<Image> bilder;
+      int diff = 0;
+      string bildefolder = {"folder1", "folder2", "folder3"};
+      string bildene = {"bilde 1", "Bilde 2", "bilde 3"};
+
+
+      //Konstruktør
+      public Brikker(int vanskelighetsgrad){
+        diff = vanskelighetsgrad;
+
+        for(int i = 0; i < bilder; i++){
+          //Hent bilde fra rescources spesifisert ut i fra
+          bilder.add(new Image./*image from rescources eller noe*/);
+        }
+      }
+
+
+
+
+
+
+
+
+    }
+}
