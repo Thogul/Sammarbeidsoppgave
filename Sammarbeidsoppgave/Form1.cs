55d6dca311a72ff6da9019e4edef2d0168ea6783
Thogul <thogul@outlook.com>
2017-03-20 10:15:01 +0100

    Added Info class
    Yello

--- Sammarbeidsoppgave/Form1.cs
+++ Sammarbeidsoppgave/Form1.cs
@@ -16,5 +16,7 @@ namespace Sammarbeidsoppgave
         {
             InitializeComponent();
         }
+
+
     }
 }
---
+++ Sammarbeidsoppgave/Info.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sammarbeidsoppgave
+
+public class Info
+{
+	public Info()
+	{
+
+
+	}
+}
