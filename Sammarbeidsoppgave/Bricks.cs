c514f742b51048d43b7703d22592741f46501f80
Thogul <thogul@outlook.com>
2017-03-20 10:54:49 +0100

    Added meat to info
    Added stuffs to le info.cs

--- Sammarbeidsoppgave/Bricks.cs
+++ Sammarbeidsoppgave/Bricks.cs
@@ -1,25 +1,35 @@
-using system;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
 
 namespace Sammarbeidsoppgave
 {
     class Bricks
     {
-      //Attributter
-      list<Image> bilder;
-      int diff = 0;
-      string bildefolder = {"folder1", "folder2", "folder3"};
-      string bildene = {"bilde 1", "Bilde 2", "bilde 3"};
+        //Attributter
+        List<Image> bilder;
+        int diff = 0;
+        string[] bildefolder = {"folder1", "folder2", "folder3"};
+        string[] bildene = {"bilde 1", "Bilde 2", "bilde 3"};
 
 
       //Konstruktør
-      public Brikker(int vanskelighetsgrad){
+    public Bricks(int vanskelighetsgrad)
+    {
         diff = vanskelighetsgrad;
 
-        for(int i = 0; i < bilder; i++){
-          //Hent bilde fra rescources spesifisert ut i fra
-          bilder.add(new Image./*image from rescources eller noe*/);
+        for(int i = 0; i < bilder.Count; i++){
+            //Hent bilde fra rescources spesifisert ut i fra
+            //bilder.Add(/*Nytt bilde */);
         }
-      }
+    }
 
 
 
--- Sammarbeidsoppgave/Info.cs
+++ Sammarbeidsoppgave/Info.cs
@@ -1,12 +1,54 @@
 ﻿using System;
 
 namespace Sammarbeidsoppgave
-
-public class Info
 {
-	public Info()
-	{
 
+    public class Info
+    {
+        private int difficulty;
+        private int cleared;
+        private int fails;
+        
+        //Constructors
+        public Info()
+        {
+            difficulty = 0;
+            cleared = 0;
+            fails = 0;
+        }
 
-	}
+        //Methods
+        public void Clear()
+        {
+            cleared++;
+        }
+
+        public void Fail()
+        {
+            fails++;
+        }
+
+        public void Reset()
+        {
+            difficulty = 0;
+            fails = 0;
+        }
+
+        //Accessors
+        public int Difficulty
+        {
+            get { return difficulty; }
+            set { difficulty = value; }
+        }
+
+        public int Cleared
+        {
+            get { return cleared; }
+        }
+
+        public int Fails
+        {
+            get { return fails; }
+        }
+    }
 }
